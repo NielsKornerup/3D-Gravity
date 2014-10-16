b5a86db7511f0818621bf4045399e506aed04a24
nielskornerup <nielsskornerup@gmail.com>
2014-10-16 09:27:37 -0500

    added first algorithm

---
+++ js/Grav.js
@@ -0,0 +1,97 @@
+var allParticles=[];
+var Num = 50;
+//var width = canvas.width;
+//var height = canvas.height;
+var scene = new THREE.Scene();
+var gravConstant = 1;
+var PI = 3.141592;
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+	var renderer;
+	if (window.WebGLRenderingContext) {
+		renderer = new THREE.WebGLRenderer();
+	}
+	else {
+		renderer = new THREE.CanvasRenderer();
+	}
+
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	document.body.appendChild(renderer.domElement);
+
+	for(var i = 0; i < Num; i++){
+		var obj = new Object();
+		obj.x = 0;
+		obj.y = 0;
+		obj.z = 0;
+		obj.radius = Math.random()*10;
+		obj.xSpeed = Math.random()*40-20;
+		obj.ySpeed = Math.random()*40-20;
+		obj.zSpeed = Math.random()*40-20;
+		var geometry = new THREE.SphereGeometry(obj.radius);
+		var material = new THREE.MeshBasicMaterial( { color: getRandomColor() } );
+		var sphere = new THREE.Mesh( geometry, material );
+		obj.material = sphere;
+		allParticles.push(obj);
+	}
+	for(var i = 0; i < Num; i++){
+		scene.add( allParticles[i].material);
+	}
+
+	camera.position.x = 0;
+	camera.position.y = 0;
+	camera.position.z = 600;
+
+var move = function () {
+	for(var x = 0; x < Num; x++){
+		for(var y = x+1; y < Num; y++){
+			var difX = (allParticles[x].x-allParticles[y].x);
+			var difY = (allParticles[x].y-allParticles[y].y);
+			var difZ = (allParticles[x].z-allParticles[y].z);
+			var dist =Math.sqrt((Math.pow(difX,2)+Math.pow(difY,2)+Math.pow(difZ,2)));
+			if(dist>5){
+				allParticles[x].xSpeed-=(difX*allParticles[y].radius*gravConstant)/Math.pow(dist,2);
+				allParticles[x].ySpeed-=(difY*allParticles[y].radius*gravConstant)/Math.pow(dist,2);
+				allParticles[y].xSpeed+=(difX*allParticles[x].radius*gravConstant)/Math.pow(dist,2);
+				allParticles[y].ySpeed+=(difY*allParticles[x].radius*gravConstant)/Math.pow(dist,2);
+				allParticles[x].zSpeed-=(difZ*allParticles[y].radius*gravConstant)/Math.pow(dist,2);
+				allParticles[y].zSpeed+=(difZ*allParticles[x].radius*gravConstant)/Math.pow(dist,2);
+				
+			}
+		}
+	}
+
+	var xSum = 0;
+	var ySum = 0;
+	var zSum = 0;
+
+	for(var i = 0; i < Num; i++){
+		allParticles[i].material.translateX(allParticles[i].xSpeed);
+		allParticles[i].material.translateY(allParticles[i].ySpeed);
+		allParticles[i].material.translateZ(allParticles[i].zSpeed);
+		allParticles[i].x+=allParticles[i].xSpeed;
+		allParticles[i].y+=allParticles[i].ySpeed;
+		allParticles[i].z+=allParticles[i].zSpeed;
+		xSum += allParticles[i].x;
+		ySum += allParticles[i].y;
+		zSum += allParticles[i].z;
+	}
+	camera.position.x = xSum/Num;
+	camera.position.y = ySum/Num;
+	camera.position.z = zSum/Num + 300;
+};
+
+var render = function () {
+	requestAnimationFrame(render);
+	renderer.render(scene, camera);
+	move();
+};
+render();
